9cf93820dfc4a6cfd8ba0d3a21bd9b409798aa5a
unknown <muzibmuzakar33@gmail.com>
2022-01-13 08:27:42 +0700

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <a class="btn btn-primary btn-sm">cek</a>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -53,7 +54,5 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
-}
+
 </style>
